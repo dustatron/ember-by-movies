92097659772bf75f631af64cd44c3dbc01e1cfa1
Dusty McCord <dustymccord@gmail.com>
2017-05-02 15:31:59 -0700

    added a vote button and a console.log

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     voteUp: function () {
+      var movieVote = this.get('post');
+      console.log("up vote " + movieVote);
     }
   }
 });
