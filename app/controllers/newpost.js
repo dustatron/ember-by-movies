8c4735409c2cc6615a89ac33252af30eb0612663
Dusty McCord <dustymccord@gmail.com>
2017-05-07 21:10:46 -0700

    added the movie db api for movie title searches

--- app/controllers/newpost.js
+++ app/controllers/newpost.js
@@ -20,21 +20,54 @@ export default Ember.Controller.extend({
     var searchValue =this.get('searchString');
     var searchLink = "http://api.themoviedb.org/3/search/movie?api_key=749c2902bde6802d4268a80d0011ab0f&query=";
     var searchString = searchLink+searchValue;
-     var Controller = this;
-      jQuery.getJSON(searchString).then(function(json) {
+    var Controller = this;
+    jQuery.getJSON(searchString).then(function(json) {
       console.log(json.results);
        Controller.set('model', json.results);
      });
    },
-   pushMovie: function(id) {
-     var newMovie = this.store.createRecord('post', {
-       title: this.get('title'),
-       date: this.get('date'),
-       image: this.get('image'),
-       overview: this.get('overview')
+   pushMovie: function(callback) {
+     var linkRange = "https://api.themoviedb.org/3/movie/"
+     var keys = '?api_key=749c2902bde6802d4268a80d0011ab0f&append_to_response=videos'
+     var movieLink = linkRange+callback+keys;
+     var jData;
+     var that = this;
+
+     let getMovie = function(callback) {
+       jQuery.getJSON(movieLink, callback);
+     }
+
+     getMovie(function(json){
+        var newMovie = that.store.createRecord('post', {
+             title: json.title,
+             tagline: json.tagline,
+             date: json.release_date,
+             image: json.poster_path,
+             overview: json.overview,
+             vote: '0',
+             timestamp: new Date().getTime()
+           });
+
+           newMovie.save();
+
      });
-     console.log(id);
-     newMovie.save();
-   }
+
+
+
+
+
+
+
+    //  var theMovie = jQuery.getJSON(movieLink).then(function(json) {
+    //    jData = json;
+     //
+    //  });
+
+    // var movie = jQuery.getJSON(movieLink).then(function(json) {
+    //   make(json);
+    //
+    // })
+
  }//actions,
+ }
 });
--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  tagline: DS.attr('string'),
   date: DS.attr('string'),
   image: DS.attr('string'),
   overview: DS.attr('string'),
